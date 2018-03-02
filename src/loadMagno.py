bc751e76aff6b25f807db224de37a496a90dde8c
William Hogg <naturesyouth@gmail.com>
2018-03-02 22:47:44 +0000

    Adds requirements.txt for installing, loadcsv now loads into pandas dataframs.

--- src/loadMagno.py
+++ src/loadMagno.py
@@ -1,36 +1,49 @@
 '''This module loads csv type magnotomerter data and preps it for further use.'''
 
-#import pandas as pd
-import pickle 
+import pandas as pd
+import numpy as np
+import datetime as dt
+import pickle
 import time
 import os
+import glob
+
 from multiprocessing import Pool, Queue
 
 thread_count = 10
 
+hfs = dt.datetime.strptime("01-01-1904", "%m-%d-%Y")  # create a ref time.
+print(hfs)
+
+
 def load_data_from_path(path):
-	'''takes a list of filenames and opens them for processing in a thread pool, returning a list of pandas data frames.'''
-	outq = Queue(maxsize=0)
-	files = glob.glob(path, recursive=True)
-	print(files)
-	
-	with Pool(processes=thread_count) as pool:
-		pool.map(process_file, glob.iglob(path, recursive=True), 10)
-		
-	print(outq.qsize())
-	return list(outq.get())
-	
+    '''takes a list of filenames and opens them for processing in a thread pool,
+     returning a list of pandas data frames.'''
+
+    outq = Queue(maxsize=0)
+
+    def en_queue_dataframe(x):
+        print(type(x))
+
+    with Pool(processes=thread_count) as pool:
+        pool.map(process_file, glob.iglob(path, recursive=True), 10,)
+
+    print(outq.qsize())
+    return list(outq.get())
+
+
 def process_file(file_name):
-	'''takes a file name and trys to load it into a pandas dataframe, converting from mac time to a datetime object.'''
-	print(file_name)
-	with open(file_name) as fd:
-		print(fd.readline())
+    '''takes a file name and trys to load it into a pandas dataframe,
+     converting from mac time to a datetime object.'''
+
+    dataframe = pd.read_csv(file_name)
+
+    dataframe[0] = dataframe[0].apply(lambda x: dt.datetime.fromtimestamp(x + hfs))
+
+    print(dataframe.describe())
+
+
+
 
 if __name__ == "__main__":
-	
-    import glob
-    
-    #files = glob.glob("data/raw/**/*.csv", recursive=True)
-
-    print (load_data_from_path( path = "data/raw/**/*.csv" ))
-    
+    print(load_data_from_path(path="data/raw/**/*.csv"))
