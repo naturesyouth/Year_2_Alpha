3f2596902332324651b100ad999ea8ce33148011
William Hogg <naturesyouth@gmail.com>
2018-03-06 02:11:16 +0000

    Changes to use threadpools for processing, also adds box plot graphing and iqr reducing.

--- src/makeTempModel.py
+++ src/makeTempModel.py
@@ -3,48 +3,55 @@
 
 import math
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
 import collections
 from multiprocessing import Pool, Queue
 
 
-class Return_Class(collections.Iterable):
-    ''' A wrapper class to pass more data to a mapable function.'''
-    def __init__(self, name=None, dataframe=None):
-        self.dataframe = dataframe
-        self.todo = Queue()
-        for i in name:
-            self.todo.put(i)
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        return (next(self.todo.get()), self.dataframe)
-
 def make_temp_data(df):
 
     temps = df.Temp.unique()
     result = []
-
-    #pool_loader = Return_Class(temps, df)
-
     for i in temps:
-        result.append(split_subframe((i, df)))
+        result.append((i,df))
+    #print(result)
 
-    for i in result:
-        print(i.describe())
-    print(len(result))
+    pool = Pool()
+
+    result = pool.map(split_subframe, result)
+
+    result = [x for x in result if x is not None]
+
+    tmp_set = pd.concat(result)
+
+    sns.boxplot(x="Temp", y="Mag", data=tmp_set)
+
+    plt.show()
+
+    #for i in result:
+    #    print(i.describe())
+    #print(len(result))
 
 
 def split_subframe(input):
     temp, dataframe = input
-    return dataframe[dataframe['Temp'] == temp]
+    dataframe = dataframe[dataframe['Temp'] == temp].sort_values(by=['Mag'])
+    x, y = dataframe.shape
+    if x >= 20:
+        return dataframe
+    else:
+        return None
 
+
+def split_iqr(dataframe):
+    qr = dataframe.quantile([.25, .75])
+    dataframe = dataframe[(dataframe['Mag'] >= qr.iloc[0]['Mag']) &
+                          (dataframe['Mag'] <= qr.iloc[1]['Mag'])]
+    return dataframe
 
 if __name__ == "__main__":
     df = pd.read_pickle("data/mag.p")
     make_temp_data(df)
-
 
 # df[df['colX'] == df['colY']]
